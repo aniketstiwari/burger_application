98001210d6cfa70f9a2dab2710d70f3786c32c00
aniketstiwari <anikaittiwari91@gmail.com>
2021-04-28 16:17:57 +0530

    [144] Using logo in our application

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import burgerLogo from '../../assets/images/burger-logo.png';
+import classes from './Logo.module.css';
+
+const Logo = () => (
+    <div className={classes.Logo}>
+        {/**Image src direct path won't work because in the end
+         * webpack will take all these files bundle them together and create
+         * a new output folder. We can't see in the development mode because
+         * all of that will happen in memory but once we do publish our app
+         * we will get a real different folder where all the optimized, compiled 
+         * and bundled assets are contained in.
+         */}
+        <img src={burgerLogo} alt="MyBurger" />
+    </div>
+);
+
+
+export default Logo;
