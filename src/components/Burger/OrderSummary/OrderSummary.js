bd52e6582c42d55218533705d3e0aa2583e8d463
aniketstiwari <anikaittiwari91@gmail.com>
2021-04-28 13:11:09 +0530

    [142] Adding the price to the order summary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -16,6 +16,7 @@ const OrderSummary = (props) => {
             <ul>
                 {ingredentSummary}
             </ul>
+            <p><strong>Total Price: {props.price.toFixed(2)} $</strong></p>
             <p>Continue to Checkout?</p>
             <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
             <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
