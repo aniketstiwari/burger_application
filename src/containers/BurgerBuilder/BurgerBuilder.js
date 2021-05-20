e985b49a5f1fe975af9f7199c81f48f451b6669c
aniketstiwari <anikaittiwari91@gmail.com>
2021-05-20 13:58:13 +0530

    Http Project [4] sending a post request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -81,7 +82,25 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You can continue');
+        //alert('You can continue');
+        //adding dummy data to firebase
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'aniket tiwari',
+                address: {
+                    street: 'test street',
+                    zipcode: '23213',
+                    country: 'test country'
+                },
+                email: 'test@test.com'
+            },
+            deliveryMethod: 'fastest'
+        }
+        axios.post('/orders.json', order)
+          .then(response => console.log(response))
+          .catch(error => console.log(error))
     }
 
     render() {
